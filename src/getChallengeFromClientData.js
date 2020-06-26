b7d006eb04504a2ca707c7fd87bda596de4fd466
Jeremias Ehnnle <jeremias.ehnle@aol.com>
2020-06-26 11:31:17 +0200

    Also use base64url to getChallengeFromClientData to be consistent

--- src/getChallengeFromClientData.js
+++ src/getChallengeFromClientData.js
@@ -1,6 +1,8 @@
+const base64url = require('base64url');
+
 exports.getChallengeFromClientData = (clientDataJSON) => {
     const clientDataBuffer = Buffer.from(clientDataJSON, 'base64');
     const clientData = JSON.parse(clientDataBuffer.toString());
     const challenge = Buffer.from(clientData.challenge, 'base64');
-    return challenge.toString('base64');
+    return base64url (challenge);
 };
